82c7140ade48ed093d9fcf9e068d4cbc99017e1f
raphaelbaere <raphaelbaere@id.uff.br>
2023-08-12 18:28:44 -0300

    Requisito 2 e 3 concluídos com sucesso!

--- src/TrybeHotel/Repository/CityRepository.cs
+++ src/TrybeHotel/Repository/CityRepository.cs
@@ -14,13 +14,24 @@ namespace TrybeHotel.Repository
         // 2. Desenvolva o endpoint GET /city
         public IEnumerable<CityDto> GetCities()
         {
-            throw new NotImplementedException();
+            var allCities = _context.Cities.Select(city => new CityDto
+            {
+                CityId = city.CityId,
+                Name = city.Name,
+            }).ToList();
+            return allCities;
         }
 
         // 3. Desenvolva o endpoint POST /city
         public CityDto AddCity(City city)
         {
-            throw new NotImplementedException();
+            _context.Cities.Add(city);
+            _context.SaveChanges();
+            return new CityDto
+            {
+                CityId = city.CityId,
+                Name = city.Name,
+            };
         }
 
     }
