235c3a78c175bb19e1b96a1ada24f0afed065705
raphaelbaere <raphaelbaere@id.uff.br>
2023-08-12 14:35:31 -0300

    Requisito 1 concluído com sucesso!

--- src/TrybeHotel/Repository/TrybeHotelContext.cs
+++ src/TrybeHotel/Repository/TrybeHotelContext.cs
@@ -6,4 +6,17 @@ public class TrybeHotelContext : DbContext, ITrybeHotelContext
 {
     public TrybeHotelContext(DbContextOptions<TrybeHotelContext> options) : base(options) { }
     public TrybeHotelContext() { }
+    public DbSet<City> Cities { get; set; } = null!;
+    public DbSet<Hotel> Hotels { get; set; } = null!;
+    public DbSet<Room> Rooms { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        // base.OnConfiguring(optionsBuilder);
+        if (!optionsBuilder.IsConfigured)
+        {
+            var connectionString = "Server=localhost;Database=TrybeHotel;User=SA;Password=TrybeHotel12!;TrustServerCertificate=True";
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+    }
 }
